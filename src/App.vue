aee5cfcbf42a34f7e0f1216939b2f0240efc44fc
gurbelov <arslanmelov@gmail.com>
2025-01-05 05:00:02 +0300

    Готовая структура компонентов

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SearchBar />
+    <ProductList />
+    <FilterBar />
+    <ProductPagination />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from '@/components/SearchBar.vue';
+import ProductList from '@/components/ProductList.vue'
+import FilterBar from '@/components/FilterBar.vue';
+import ProductPagination from '@/components/ProductPagination.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    SearchBar,
+    ProductList,
+    FilterBar,
+    ProductPagination
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
